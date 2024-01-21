863e000239db4abb9ffa047601ecc708196b2487
Buspe <93484404+AM-Dominik@users.noreply.github.com>
2024-01-21 22:13:29 +0100

    21.01.2024 Usuwanie prefiksu base64

--- base64encoding.py
+++ base64encoding.py
@@ -3,16 +3,20 @@ import base64
 from PIL import Image
 from io import BytesIO
 
+def remove_base64_prefix(encoded_bytes):
+    prefix = b"data:image/jpeg;base64,"
+    return encoded_bytes[len(prefix):] if encoded_bytes.startswith(prefix) else encoded_bytes
+
 def encodeImage(imageData):
     return base64.b64encode(imageData)
 
-def decodeImage(imageData):  
+def decodeImage(imageData):
     imageData = Image.open(BytesIO(base64.b64decode(imageData)))
     return imageData
 
 def readFileData(file_name):
     file = open(file_name, "rb")
-    return file.read()
+    return remove_base64_prefix(file.read())
 
 def writeFileData(file_name, data): #data - dane do zapisania
     file = open(file_name, 'w')
